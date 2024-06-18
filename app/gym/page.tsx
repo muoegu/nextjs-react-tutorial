574c9a147203f8318d5e0b9a93aef23a09e04ef3
Micah Kitsunai <kmicah1794@gmail.com>
2024-06-18 23:04:17 +0800

    ✨ update

--- app/gym/page.tsx
+++ app/gym/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 
-import { Title, NumberInput, Text } from "@mantine/core";
+import { Title, NumberInput, Text, Container } from "@mantine/core";
 import { useState } from "react";
 
 const GymClient = () => {
     const [value, setValue] = useState<number>(1);
     const month_fee = 1299;
-    const per_day = month_fee / value / 2;
-    const price_JPY = per_day * 4.872;
+    const month_fee_JPY = (month_fee * 4.872).toFixed(1);
+    const per_day = (month_fee / value / 2).toFixed(1); // `toFixed`で文字列になる
+    const per_day_JPY = (parseFloat(per_day) * 4.872).toFixed(1); // 数値に変換してから再計算
 
     return (
-        <>
+        <Container px={0} size="70%">
             <Title m={20}>World Gym Calculator</Title>
             <NumberInput
                 label="How many times did you go to the gym this month?"
                 value={value}
-                onChange={(val) => setValue(typeof val === 'number' ? val : 0)}
+                onChange={(val) => setValue(typeof val === 'number' && val > 0 ? val : 1)} // 型ガード
+                min={1} // 値が1未満にならないように設定           
             />
-            <Text>Month Fee: {month_fee.toLocaleString()}</Text>
-            <Text>Estimated price per day:{per_day}</Text>
-            <Text>JPY ≒ ￥{price_JPY}</Text>
-        </>
+            <Text mt={20}>Month Fee: TWD {month_fee.toLocaleString()}</Text>
+            <Text>JPY ≒ ¥{month_fee_JPY}</Text>
+            <Text>Estimated price per day: TWD {per_day}</Text>
+            <Text>JPY ≒ ¥{per_day_JPY}</Text>
+            <Text mt={20}>サウナ1回使用量 ¥{per_day_JPY}</Text>
+            <Text>ジム1回使用量 ¥{per_day_JPY}</Text>
+        </Container>
     );
 }
 
