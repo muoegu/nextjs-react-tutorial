d836d3c3a88393889d75b13dd9e7f7e79a197ae3
Micah Kitsunai <kmicah1794@gmail.com>
2024-06-18 21:19:45 +0800

    ✨ add Gym Calculater

---
+++ app/gym/page.tsx
@@ -0,0 +1,17 @@
+
+
+const month_fee = 1299;
+
+const Gym = () => {
+    return (
+        <>
+            <h2>World Gym Calculater</h2>
+            <input type="number" />
+            <p>Month Fee: {month_fee.toLocaleString()}</p>
+        </>
+
+    );
+}
+
+
+export default Gym;
